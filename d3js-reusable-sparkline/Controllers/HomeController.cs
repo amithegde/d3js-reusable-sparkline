90941dcf0bd5fd675a6c1a17cdc6d2fe61a69a8b
amithegde <amits.hegde@gmail.com>
2014-03-05 17:59:27 -0800

    Support for missing data
    line function updated to support missing y axis data in the collection.

--- d3js-reusable-sparkline/Controllers/HomeController.cs
+++ d3js-reusable-sparkline/Controllers/HomeController.cs
@@ -23,25 +23,30 @@ namespace d3js_reusable_sparkline.Controllers
         {
             var list = new List<object>();
 
-            list.Add(new { Date = DateTime.Parse("Dec 18, 2013"), Close = 1084.75 });
-            list.Add(new { Date = DateTime.Parse("Dec 17, 2013"), Close = 1069.86 });
-            list.Add(new { Date = DateTime.Parse("Dec 16, 2013"), Close = 1072.98 });
-            list.Add(new { Date = DateTime.Parse("Dec 13, 2013"), Close = 1060.79 });
-            list.Add(new { Date = DateTime.Parse("Dec 12, 2013"), Close = 1069.96 });
-            list.Add(new { Date = DateTime.Parse("Dec 11, 2013"), Close = 1077.29 });
-            list.Add(new { Date = DateTime.Parse("Dec 10, 2013"), Close = 1084.66 });
-            list.Add(new { Date = DateTime.Parse("Dec 9, 2013"), Close = 1078.14 });
-            list.Add(new { Date = DateTime.Parse("Dec 6, 2013"), Close = 1069.87 });
-            list.Add(new { Date = DateTime.Parse("Dec 5, 2013"), Close = 1057.34 });
-            list.Add(new { Date = DateTime.Parse("Dec 4, 2013"), Close = 1058.18 });
-            list.Add(new { Date = DateTime.Parse("Dec 3, 2013"), Close = 1053.26 });
-            list.Add(new { Date = DateTime.Parse("Dec 2, 2013"), Close = 1054.48 });
-            list.Add(new { Date = DateTime.Parse("Nov 29, 2013"), Close = 1059.59 });
-            list.Add(new { Date = DateTime.Parse("Nov 27, 2013"), Close = 1063.11 });
-            list.Add(new { Date = DateTime.Parse("Nov 26, 2013"), Close = 1058.41 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 18, 2013"), Close = 1084.75 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 17, 2013"), Close = 1069.86 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 16, 2013"), Close = 1072.98 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 13, 2013"), Close = 1060.79 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 12, 2013"), Close = 1069.96 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 11, 2013"), Close = null });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 10, 2013"), Close = null });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 9, 2013"), Close = 1078.14 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 6, 2013"), Close = 1069.87 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 5, 2013"), Close = 1057.34 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 4, 2013"), Close = 1058.18 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 3, 2013"), Close = 1053.26 });
+            list.Add(new Stock { Date = DateTime.Parse("Dec 2, 2013"), Close = 1054.48 });
+            list.Add(new Stock { Date = DateTime.Parse("Nov 29, 2013"), Close = 1059.59 });
+            list.Add(new Stock { Date = DateTime.Parse("Nov 27, 2013"), Close = 1063.11 });
+            list.Add(new Stock { Date = DateTime.Parse("Nov 26, 2013"), Close = 1058.41 });
 
             return Json(list, JsonRequestBehavior.AllowGet);
         }
+    }
 
+    public class Stock
+    {
+        public DateTime Date { get; set; }
+        public double? Close { get; set; }
     }
 }
